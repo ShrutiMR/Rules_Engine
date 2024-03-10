0d9791f787665eb0b5c8dc7e2eae8637e2bc89b5
ShrutiRM97 <98553136+ShrutiRM97@users.noreply.github.com>
2024-03-09 19:15:37 -0600

    Update rule in db logic added

--- FrontEndService.py
+++ FrontEndService.py
@@ -10,10 +10,20 @@ rules_db_url = 'http://127.0.0.1:9001/rules'
 def home():
     return render_template('index.html')
 
-def send_request_to_rules_db(json_input_data, user_input):
+def send_request_to_rules_db(json_input_data, user_input, rule_id=None):
     try:
         # Make HTTP POST request to RulesDBService
-        response = requests.post(f'{rules_db_url}/{user_input}', json=json_input_data)
+        print(user_input)
+        print(json_input_data)
+        if user_input == 'create':
+            response = requests.post(f'{rules_db_url}/{user_input}', json=json_input_data)
+        elif user_input == 'update':
+            print('enter')
+            response = requests.put(f'{rules_db_url}/{user_input}/{rule_id}', json=json_input_data)
+        elif user_input == 'delete':
+            response = requests.delete(f'{rules_db_url}/{user_input}/{rule_id}', json=json_input_data)
+        elif user_input == 'get':
+            response = requests.get(f'{rules_db_url}/{user_input}/{rule_id}', json=json_input_data)
 
         # Check the response status
         if response.ok:
@@ -33,9 +43,13 @@ def process_rules():
         # Get input data from the form
         user_input = request.form.get('action', '').lower()
         json_input_data = request.form.get('rule_data', '')
+        rule_id = None
+        
+        if user_input in ['update', 'delete', 'get']:
+            rule_id = request.form.get('rule_id')
 
         # Call the Front-End Service to process rules
-        response = send_request_to_rules_db(json_input_data, user_input)
+        response = send_request_to_rules_db(json_input_data, user_input, int(rule_id) if rule_id is not None else None)
         return jsonify(response), 200
 
     except Exception as e:
--- RulesDBService.py
+++ RulesDBService.py
@@ -12,8 +12,7 @@ class RulesDBService:
     def createRule(self, json_input):
         format_rule = json.loads(json_input)
 
-        condition_rule = format_rule["condition"].split(' ')
-        csv_file_path = "rules_engine_db\RulesFile.csv"
+        csv_file_path = "rules_engine_db/RulesFile.csv"
 
         existing_rows = []
         with open(csv_file_path, mode='r', newline='') as csv_file:
@@ -25,10 +24,20 @@ class RulesDBService:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(csv_row)
 
-        print(f"The rule has been appended to {csv_file_path}.")
+    def updateRule(self, rule_id, json_input):
+        format_rule = json.loads(json_input)
 
-    def updateRule(self, json_input):
-        print('Update rule')
+        csv_file_path = "rules_engine_db/RulesFile.csv"
+
+        existing_rows = []
+        with open(csv_file_path, mode='r', newline='') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            existing_rows = list(csv_reader)
+
+        existing_rows[int(rule_id)-1] = [rule_id, format_rule["name"], format_rule["condition"], format_rule["action"]]
+        with open(csv_file_path, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerows(existing_rows)
 
     def getRule(self, json_input):
         print('Get rule')
@@ -41,20 +50,29 @@ rules_service = RulesDBService()
 #API endpoint for health check
 @app.route('/rules', methods=['GET'])
 def check_api():
-    # print('Health check Success!')
     return jsonify({'status': 'ok', 'message': 'Health check success!'}), 200
 
 # API endpoint for creating a rule
 @app.route('/rules/create', methods=['POST'])
 def create_rule():
     try:
-        rule_data = request.json
-        rules_service.createRule(rule_data)
+        rule_insert_data = request.get_json()
+        rules_service.createRule(rule_insert_data)
         return jsonify({'message': 'Rule created successfully'}), 201
     except Exception as e:
         traceback.print_exc() #to log for exception details
         return jsonify({'error': str(e)}), 500
-    
+
+# API endpoint for updating a rule
+@app.route('/rules/update/<id>', methods=['PUT'])
+def update_rule(id):
+    try:
+        rule_update_data = request.get_json()
+        rules_service.updateRule(id, rule_update_data)
+        return jsonify({'message': 'Rule updated successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9001)
 
