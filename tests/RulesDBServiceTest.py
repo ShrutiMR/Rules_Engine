cefc4775b239cfe52817c364e2206a09d5fcf000
ShrutiRM97 <98553136+ShrutiRM97@users.noreply.github.com>
2024-03-12 17:26:01 -0500

    test cases for Rules DB Service

--- tests/RulesDBServiceTest.py
+++ tests/RulesDBServiceTest.py
@@ -20,43 +20,75 @@ class RulesDBServiceTest(unittest.TestCase):
         self.client = self.app.test_client()
 
     def testCreateRuleAlreadyExists(self):
-        rule_data = {
-            "name":"incomeRule", 
-            "condition":"income > 8000", 
-            "action":"initial_state"
-        }
-        
-        response = self.service.createRule(json.dumps(rule_data))
-        resp_name = rule_data['name']
-        expected_msg = f"Rule with name '{resp_name}' already exists."
-        self.assertEqual(str(response), expected_msg)
+        try:
+            rule_data = {
+                    "name": "incomeRule", 
+                    "condition": [
+                        {
+                        "key": "income",
+                        "constraints": "<",
+                        "value": 10000
+                        }
+                    ],
+                    "action": "initial_state"
+                }
+            resp_name = rule_data["name"]
+            response = self.service.createRule(json.dumps(rule_data))
+            expected_msg = f"Rule with name '{resp_name}' already exists."
+            self.assertEqual(str(response), expected_msg)
+        except ValueError as ve:
+            expected_msg = f"Rule with name '{resp_name}' already exists."
+            self.assertEqual(str(ve), expected_msg)
+        except Exception as e:
+            self.assertEqual(str(e), expected_msg)
 
 
     def testUpdateRuleDoesNotExist(self):
-        rule_id = '4'
-        rule_data = {
-            "name":"incomeRule",
-            "condition":"income < 10000",
-            "action":"initial_state"
-        }
+        try:
+            rule_id = '4'
+            rule_data = {
+                    "name": "incomeRule", 
+                    "condition": [
+                        {
+                        "key": "income",
+                        "constraints": "<",
+                        "value": 10000
+                        }
+                    ],
+                    "action": "initial_state"
+                }
 
-        response = self.service.updateRule(rule_id, json.dumps(rule_data))
-        expected_msg = f"Rule id - {rule_id} does not exist!"
-        self.assertEqual(str(response), expected_msg)
+            response = self.service.updateRule(rule_id, json.dumps(rule_data))
+            expected_msg = f"Rule id - {rule_id} does not exist!"
+            self.assertEqual(str(response), expected_msg)
+        except ValueError as ve:
+            expected_msg = f"Rule id - {rule_id} does not exist!"
+            self.assertEqual(str(ve), expected_msg)
+        except Exception as e:
+            self.assertEqual(str(e), expected_msg)
 
     def testGetRule(self):
-        rule_id = '1'
-        
-        response = self.service.getRule(rule_id)
-        expected_msg = f"['1', 'ageRule', 'age < 100', 'initial_state']"
-        self.assertEqual(str(response), expected_msg)
+        try:
+            rule_id = '1'
+            response = self.service.getRule(rule_id)
+            expected_msg = ['1', 'ageRule', "[{'key': 'age', 'constraints': '<', 'value': 100}]", 'initial_state']
+            self.assertEqual(response, expected_msg)
+        except ValueError as ve:
+            self.assertEqual(str(ve), expected_msg)
+        except Exception as e:
+            self.assertEqual(str(e), expected_msg)
 
     def testDeleteRuleDoesNotExist(self):
-        rule_id = '5'
-
-        response = self.service.deleteRule(rule_id)
-        expected_msg = f"Rule id - {rule_id} does not exist!"
-        self.assertEqual(str(response), expected_msg)
+        try:
+            rule_id = '5'
+            response = self.service.deleteRule(rule_id)
+            expected_msg = f"Rule id - {rule_id} does not exist!"
+            self.assertEqual(str(response), expected_msg)
+        except ValueError as ve:
+            expected_msg = f"Rule id - {rule_id} does not exist!"
+            self.assertEqual(str(ve), expected_msg)
+        except Exception as e:
+            self.assertEqual(str(e), expected_msg)
 
 if __name__ == '__main__':
     unittest.main()
